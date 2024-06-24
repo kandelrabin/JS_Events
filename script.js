b4d9a1f2635e9c988da7998c2a2f6d1a58a0560c
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-24 16:36:30 +0100

    wrote and tested delete function

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const enterButton = document.querySelector("#enter");
 const todoForm = document.querySelector("#todo-form");
-
+const list = document.querySelector("#list");
 
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const newListItem = document.createElement("li");
     newListItem.innerText = event.target["new-todo"].value;
-    todoForm.appendChild(newListItem);
-});
+    newListItem.id = newListItem.innerText;
+    deleteButton = document.createElement("button");
+    deleteButton.textContent = `delete ${newListItem.innerText}`;
+    newListItem.appendChild(deleteButton);
+    list.appendChild(newListItem);
 
+    deleteButton.addEventListener("click", () => {
+        list.removeChild(newListItem);
+    });
+    
+});
